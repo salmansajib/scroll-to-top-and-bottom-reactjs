043be376115340ecd4daa72281976c7e43a7dbc7
Salman-sajib <sfsajib@gmail.com>
2024-04-29 13:14:26 +0600

    added the functionality to go to the top and bottom of the page

--- src/components/ScrollToTopAndBottom.jsx
+++ src/components/ScrollToTopAndBottom.jsx
@@ -1,10 +1,26 @@
+import { useRef } from 'react';
 import UseFetch from './UseFetch';
 
 function ScrollToTopAndBottom() {
   const { data, pending, error } = UseFetch(
-    'https://dummyjson.com/products',
+    'https://dummyjson.com/products?limit=100',
     {}
   );
+  const bottomRef = useRef(null);
+
+  function handleScrollToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  function handleScrollToBottom() {
+    bottomRef.current.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
 
   if (error) {
     return <h1>Error occured! Please try again.</h1>;
@@ -18,7 +34,7 @@ function ScrollToTopAndBottom() {
     <div>
       <h1>Scroll To Top And Bottom Feature</h1>
       <h3>This is the top section</h3>
-      <button>Scroll To Bottom</button>
+      <button onClick={handleScrollToBottom}>Scroll To Bottom</button>
       <ul>
         {data && data.products && data.products.length
           ? data.products.map((item) => (
@@ -34,7 +50,8 @@ function ScrollToTopAndBottom() {
             ))
           : null}
       </ul>
-      <button>Scroll To Top</button>
+      <button onClick={handleScrollToTop}>Scroll To Top</button>
+      <span ref={bottomRef}></span>
       <h3>This is the bottom of this page</h3>
     </div>
   );
